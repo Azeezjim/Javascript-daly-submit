f52705199612eb8208458c9fbc91cd75f484de81
Azeezjim <azeexjim@gmail.com>
2023-01-29 06:18:10 -1100

    Javascript Day 1 2nd

--- day-01/main.js
+++ day-01/main.js
@@ -1,1 +1,7 @@
-const cursor = document.querySelector('cursor')
+const cursor = document.querySelector('cursor');
+
+window.addEventListener("mousemove", e => {
+  const cursorLeft = e.pageX + "px";
+  const cursorTop = e.pageX + "px";
+
+})
