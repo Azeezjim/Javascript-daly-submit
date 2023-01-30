cc199dfa638b4dee6ae9970b4b8cd059ac5f0d8f
Azeezjim <azeexjim@gmail.com>
2023-01-29 22:19:36 -1100

    Javascript Day 1 commit 6

--- day-01/main.js
+++ day-01/main.js
@@ -3,10 +3,11 @@ const cursor = document.querySelector('cursor');
 window.addEventListener("mousemove", e => {
   cursor.style.left = e.pageX + "px";
   cursor.style.top = e.pageX + "px";
-  cursor.setAttribute{'ata-fromTop', }
+  cursor.setAttribute('data-fromTop', ((cursor.offsetTop - scrollY)))
 });
 
 window.addEventListener("mousemove", e => {
+  const fromTop = cursor.getAttribute('data-fromTop')
   cursor.style.riight = scrollX + "px";
   cursor.style.top = scrollY + "px";
  })
