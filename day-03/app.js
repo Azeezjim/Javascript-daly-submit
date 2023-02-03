0377156d9cc738371bbc1c6660addc96cfe0ce64
Azeezjim <azeexjim@gmail.com>
2023-02-03 05:56:48 -1100

    Chalange of the day 3 commit 2

--- day-03/app.js
+++ day-03/app.js
@@ -1,2 +1,14 @@
 const button = document.querySelector('.container button') ;
-const jokeText = document.querySelector('.container p');
+const jokeText = document.querySelector('.container .joke');
+
+button.addEventListener('click', getJoke)
+
+async function getJoke() {
+  const jokeData = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+  const dataObj = await jokeData.json();
+  jokeText.innerHTML = dataObj.joke;  
+}
