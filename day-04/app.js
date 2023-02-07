e5f172876eb4b38ad2da19b297b64ad2edc49dbf
Azeezjim <azeexjim@gmail.com>
2023-02-06 23:13:48 -1100

    Chalange of the day 4 commit 2

--- day-04/app.js
+++ day-04/app.js
@@ -2,11 +2,17 @@ const form = document.querySelector('form');
 const factDiv = document.querySelector('.number-fact');    
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const number = e.querySelector('input[type = "number"]').value; 
-  const loading = 'Wait a little bit';
+  e.preventDefault();
+  const number = e.target.querySelector('input[type="number"]').value; 
+  const loading = 'Wait a little bit...-';
   factDiv.innerHTML = loading;
   const baseURL = 'http://numbersapi.com/'
-  fetch(baseURL + number)
-    .then(response => console.log("responce", response))
+  fetch(baseURL + number, {
+    method: 'GET',
+    headers: {
+      'x-requested-with': 'text/pline'
+    }
+  })
+    .then(response => response.text())
+    .then(text => factDiv.innerHTML = text)
 })
